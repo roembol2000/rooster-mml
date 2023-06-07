726c166f6cbacf4b793467f59e3fb8daf3fc81f8
Roembol <36731661+roembol2000@users.noreply.github.com>
2023-06-07 16:59:52 +0200

    Implement interface dark mode

--- client/src/components/Faq.jsx
+++ client/src/components/Faq.jsx
@@ -3,8 +3,10 @@ import PropTypes from "prop-types";
 const FaqEntry = ({ question, answer }) => {
   return (
     <div>
-      <h2 className="text-xl font-semibold text-neutral-700">{question}</h2>
-      <p>{answer}</p>
+      <h2 className="text-xl font-semibold text-neutral-700 dark:text-white">
+        {question}
+      </h2>
+      <p className="text-neutral-700 dark:text-white">{answer}</p>
     </div>
   );
 };
@@ -12,18 +14,18 @@ const FaqEntry = ({ question, answer }) => {
 const Faq = () => {
   return (
     <div className="max-w-xl lg:max-w-none">
-      <h1 className="mb-2 text-2xl font-semibold">FAQ</h1>
+      <h1 className="mb-2 text-2xl font-semibold dark:text-white">FAQ</h1>
       <div className="space-y-2">
         <FaqEntry
           question="Wat gebeurt er met mijn inloggegevens?"
           answer="Omdat een wachtwoord nodig is om het rooster te bekijken, moet je je wachtwoord hier invullen. Het wachtwoord wordt doorgestuurd naar de server, die vervolgens in jou naam het rooster ophaalt."
         />
-        <hr />
+        <hr className="dark:border-neutral-700" />
         <FaqEntry
           question="Wordt mijn wachtwoord opgeslagen?"
           answer="Nee, je wachtwoord wordt niet opgeslagen. Daarom moet je ook telkens opnieuw inloggen. Als je echt zeker wilt zijn dat je wachtwoord veilig is, kun je de applicatie op je eigen server draaien."
         />
-        <hr />
+        <hr className="dark:border-neutral-700" />
         <FaqEntry
           question="Hoe werkt dit?"
           answer="Je kunt de volledige broncode van deze website vinden op GitHub, zie de footer."
--- client/src/components/Header/SuggestionsEntry.jsx
+++ client/src/components/Header/SuggestionsEntry.jsx
@@ -4,9 +4,9 @@ const SuggestionsEntry = ({ name, clickHandler }) => {
   return (
     <div
       onClick={clickHandler}
-      className="flex items-center h-16 px-6 bg-white border-b cursor-pointer md:justify-center border-neutral-300 hover:bg-neutral-100"
+      className="flex items-center h-16 px-6 bg-white border-b cursor-pointer dark:bg-neutral-800 md:justify-center border-neutral-300 dark:border-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-700"
     >
-      <div className="md:w-[32rem] md:p-6">{name}</div>
+      <div className="md:w-[32rem] md:p-6 dark:text-white">{name}</div>
     </div>
   );
 };
